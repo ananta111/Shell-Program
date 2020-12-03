e6dfc34aec6ca0c5176765f952ea23e614878d55
Ananta <ananta.thapaliya1@yahoo.com>
2020-12-03 15:56:45 -0500

    adding files to repo

---
+++ cmake-build-debug/CMakeFiles/SuperShell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SuperShell.dir/main.c.o"
+  "SuperShell.pdb"
+  "SuperShell"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SuperShell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
